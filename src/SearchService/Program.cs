ea481657651bdc02dedac834ba55c9b591b31ce8
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-02-20 17:22:35 +0530

    search service - seeding

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,5 +13,14 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    await DbInitializer.InititDb(app);
+}
+catch (System.Exception e)
+{
+    Console.WriteLine(e);
+}
 
 app.Run();
