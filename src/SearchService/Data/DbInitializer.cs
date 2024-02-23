ed60f4333d75107a78bf99bf36a2c5b0c321bc90
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2024-02-24 01:40:56 +0530

    Search service communication with auction service using polly

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -17,14 +17,24 @@ public class DbInitializer
             .CreateAsync();
 
         var count = await DB.CountAsync<Item>();
-        if (count == 0)
-        {
-            Console.WriteLine("No Data - will attempt to seed");
-            var itemData = await File.ReadAllTextAsync("Data/auctions.json");
-            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+        // if (count == 0)
+        // {
+        //     Console.WriteLine("No Data - will attempt to seed");
+        //     var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+        //     var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        //     var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
 
-            await DB.SaveAsync(items);
-        }
+        //     await DB.SaveAsync(items);
+        // }
+
+        using var scope = app.Services.CreateScope();
+
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+
+        var items = await httpClient.GetItemsForSearchDb();
+        
+        Console.WriteLine(items.Count + " returned from the auction service");
+
+        if (items.Count > 0) await DB.SaveAsync(items);
     }
 }
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,5 +1,7 @@
 using MongoDB.Driver;
 using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -14,13 +17,21 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
-{
-    await DbInitializer.InititDb(app);
-}
-catch (System.Exception e)
-{
-    Console.WriteLine(e);
-}
+app.Lifetime.ApplicationStarted.Register(async () => {
+    try
+    {
+        await DbInitializer.InititDb(app);
+    }
+    catch (System.Exception e)
+    {
+        Console.WriteLine(e);
+    }
+});
+
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy() 
+    => HttpPolicyExtensions.HandleTransientHttpError()                          // errors that will not happen in future
+    .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)      // if service unavailbale
+    .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));                    // retry in async once in 3 seconds
